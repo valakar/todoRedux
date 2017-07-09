e4c71833cfa613c2116f8c8d674e4fcb9edf9e1e
vik <valakar@gmail.com>
2017-07-09 21:39:19 +0300

    add provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import store from './todos/store/Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
 
 
 registerServiceWorker();
