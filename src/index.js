c0a8ce220ea82f464f85c5ea990f9b4e03d06367
vik <valakar@gmail.com>
2017-07-09 21:09:14 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import store from './todos/store/Store';
 
-const render = () => ReactDOM.render(<App {...store.getState()}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-store.subscribe(render);
-render();
 
 registerServiceWorker();
