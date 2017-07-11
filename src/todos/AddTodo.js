3c84171b3bf598caa1a252a55ed973c06889fe2d
vik <valakar@gmail.com>
2017-07-11 18:40:16 +0300

    Use Connect for AddTodo

--- src/todos/AddTodo.js
+++ src/todos/AddTodo.js
@@ -1,34 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TODO_ACTIONS } from './actions/todo';
-import PropTypes from 'prop-types';
+import { connect } from "react-redux";
 
 let nextTodoId = 0;
 
-class AddTodo extends Component {
-    render() {
-        const { store } = this.context;
-        let input;
-        return (
-            <div>
-                <input type="text" ref={node => {
-                    input = node;
-                }}/>
-                <button onClick={() => {
-                    store.dispatch({
-                        type: TODO_ACTIONS.ADD_TODO,
-                        id: nextTodoId++,
-                        text: input.value
-                    });
-                    input.value = '';
-                }}>add todo
-                </button>
-            </div>
-        );
-    }
-}
-
-AddTodo.contextTypes = {
-    store: PropTypes.object
+let AddTodo = ({ dispatch }) => {
+    let input;
+    return (
+        <div>
+            <input type="text" ref={node => {
+                input = node;
+            }}/>
+            <button onClick={() => {
+                dispatch({
+                    type: TODO_ACTIONS.ADD_TODO,
+                    id: nextTodoId++,
+                    text: input.value
+                });
+                input.value = '';
+            }}>add todo
+            </button>
+        </div>
+    );
 };
+
+AddTodo = connect()(AddTodo);
 
 export default AddTodo;
