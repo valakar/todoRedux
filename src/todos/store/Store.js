612f5486158c7b689b26a9ec484d7ebc8d76461c
vik <valakar@gmail.com>
2017-07-13 18:12:16 +0300

    Persisting the State to the Local Storage: use throttle

--- src/todos/store/Store.js
+++ src/todos/store/Store.js
@@ -1,13 +1,14 @@
 import { createStore } from 'redux';
 import { reducer } from "../reducers";
 import { loadState, saveState } from "../data/localStorage";
+import { throttle } from "lodash";
 
 const persistedState = loadState();
 
 const store = createStore(reducer, persistedState);
 
-store.subscribe(() => saveState({
+store.subscribe(throttle(() => saveState({
     todos: store.getState().todos
-}));
+})), 1000);
 
 export default store;
